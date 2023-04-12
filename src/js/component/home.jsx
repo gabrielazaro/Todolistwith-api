cbbe595825e6caa3c1c1fe24603e89ab4bf45e3c
Gabriela Zambrano Rodríguez <gabrielazaro97@gmail.com>
2023-04-12 19:32:08 +0000

    version miercoles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,41 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [tareas, setTareas] = useState([]);
+  useEffect(() =>
+  // aquí busco mis todos de la API
+  fetch('https://assets.breatheco.de/apis/fake/todos/user/gabrielazaro')
+	  .then(r => r.json())
+	  .then(data => setTareas(data)) // aqui reseteo la variable tasks con los datos entrantes.
+, []);
+  return (
+    <div className="container-fluid">
+      <h1>TO DO'S</h1>
+      <input
+        type="text"
+        placeholder="What needs to be done?"
+      />
+      <ul>
+	  {tareas.map(t => <li>{t.label}</li>)}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
+
+
+// fetch('https://example.com/users', {
+//   method: 'PUT', // or 'POST'
+//   body: JSON.stringify(data), // los datos pueden ser una `cadena` o un {objeto} que proviene de algún lugar más arriba en nuestra aplicación
+//   headers:{
+//     'Content-Type': 'application/json'
+//   }
+// })
+// .then(res => {
+// 	if (!res.ok) throw Error(res.statusText);
+// 	return res.json();
+// })
+// .then(response => console.log('Success:', response))
+// .catch(error => console.error(error));
