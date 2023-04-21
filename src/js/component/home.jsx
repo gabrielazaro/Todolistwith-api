07acb362995c8edfd07ea56952b5c9414e6a6e40
Gabriela Zambrano Rodríguez <gabrielazaro97@gmail.com>
2023-04-21 10:47:59 +0000

    Terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,40 +2,90 @@ import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
+  const [input, setInput] = useState("");
   const [tareas, setTareas] = useState([]);
-  useEffect(() =>
-  // aquí busco mis todos de la API
-  fetch('https://assets.breatheco.de/apis/fake/todos/user/gabrielazaro')
-	  .then(r => r.json())
-	  .then(data => setTareas(data)) // aqui reseteo la variable tasks con los datos entrantes.
-, []);
+  const getAllTareas = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/gabrielazaro")
+      .then((response) => response.json())
+      .then((data) => {
+        setTareas(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+  useEffect(() => {
+    getAllTareas();
+  }, []);
+
+  const agregarTarea = () => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let agregar = tareas.concat({
+      label: input,
+      done: false,
+    });
+
+    var requestOptions = {
+      method: "PUT",
+      headers: myHeaders,
+      body: JSON.stringify(agregar),
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://assets.breatheco.de/apis/fake/todos/user/gabrielazaro",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => getAllTareas(result))
+      .catch((error) => console.log("error", error));
+  setInput("")};
+
+  const borrarTarea = (t) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let borrar = tareas.filter((tar) => tar !== t);
+// Puedes utilizar el index o el elemento. tar/ t es el li de abajo 
+    var requestOptions = {
+      method: "PUT",
+      headers: myHeaders,
+      body: JSON.stringify(borrar),
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://assets.breatheco.de/apis/fake/todos/user/gabrielazaro",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => getAllTareas(result))
+      .catch((error) => console.log("error", error));
+  };
+
+  // Hay que traer nuevamente la bbdd al front para poder mostrar la informacion que hemos agregado en el put
   return (
     <div className="container-fluid">
       <h1>TO DO'S</h1>
       <input
         type="text"
         placeholder="What needs to be done?"
+        value={input}
+        onChange={(e) => {setInput(e.target.value)}}
       />
+      {/* El input siempre es lo mismo en cuanto a variable de estado y cambio de variable con onchange */}
+      <button className="bg-light" onClick={agregarTarea}>
+        Add
+      </button>
       <ul>
-	  {tareas.map(t => <li>{t.label}</li>)}
+        {tareas.map((t, index) => (
+          <li key={index}>{t.label} <button onClick={() => borrarTarea(t)}>Eliminar</button></li>
+        ))}
       </ul>
     </div>
   );
 };
 
 export default Home;
-
-
-// fetch('https://example.com/users', {
-//   method: 'PUT', // or 'POST'
-//   body: JSON.stringify(data), // los datos pueden ser una `cadena` o un {objeto} que proviene de algún lugar más arriba en nuestra aplicación
-//   headers:{
-//     'Content-Type': 'application/json'
-//   }
-// })
-// .then(res => {
-// 	if (!res.ok) throw Error(res.statusText);
-// 	return res.json();
-// })
-// .then(response => console.log('Success:', response))
-// .catch(error => console.error(error));
